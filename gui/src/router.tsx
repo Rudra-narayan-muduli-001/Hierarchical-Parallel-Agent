76e3300034f3c5ec70d7f8edefbeb0ee00fdbb65
Rudra Narayan Muduli <rudranarayanmuduli001@gmail.com>
2026-08-31 19:39:36 +0530

    Update router.tsx

--- gui/src/router.tsx
+++ gui/src/router.tsx
@@ -1,9 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Layout } from './components/Layout';
+import { createBrowserRouter } from 'react-router-dom'
+import { Layout } from './components/Layout'
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-  },
-]);
+export const router = createBrowserRouter([{ path: '/', element: <Layout /> }])
