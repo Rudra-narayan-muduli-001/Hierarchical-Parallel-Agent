1b771f62bcc14e0a64d0a23ad69b95d37cc2147c
Rudra Narayan Muduli <rudranarayanmuduli001@gmail.com>
2026-08-31 19:38:53 +0530

    Update main.tsx

--- gui/src/main.tsx
+++ gui/src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-import './styles/global.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
